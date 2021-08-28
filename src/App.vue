f2c98ffe9de2efea2c5fe1140fb75df85eda48eb
KeisukeTsuji <kei.iwmmd@gmail.com>
2021-08-28 12:24:49 +0900

    Add test code

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
 import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
 
+console.log('hello world')
+
 export default defineComponent({
   name: 'App',
   components: {
